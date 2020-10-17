b8eba4171268966c358b5a90e88057a4a7d8da8f
Girverius <gsmac1224@gmail.com>
2020-10-17 15:53:02 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//we need select everything from html and store in var
+//need a function to start quiz
+//first add class hide then remove class hide from second div
+//start timer
+//run second function quiz/questions: inside this function render questions with array of objects with questions
+//from the array use for each to render questions
+//keep score on choices selection. if correct add score. if incorrect deduct time
+//end quiz function, inside function calculate score and end timer(if and else 0 run end)
+//function for saving score and push to localstorage
